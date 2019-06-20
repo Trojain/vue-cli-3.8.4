3acec90a77e1175d213b880ed66e88c3a706701e
xiabing <550392236@qq.com>
2019-06-20 10:57:46 +0800

    update

--- config/index.js
+++ config/index.js
@@ -22,8 +22,8 @@ module.exports = {
 
         // Various Dev Server settings
         host: '0.0.0.0', // can be overwritten by process.env.HOST
-        port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-        autoOpenBrowser: true,
+        port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+        autoOpenBrowser: false,
         errorOverlay: true,
         notifyOnErrors: true,
         poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
